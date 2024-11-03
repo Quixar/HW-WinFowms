3e01b2626abb690d3986d586474a02d2b39ecc39
Quixar <evgenmart01@gmail.com>
2024-11-03 15:48:43 +0200

    hw done

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,18 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            label2 = new Label();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(60, 148);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Click";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(309, 450);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Label label2;
+        private Button button1;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,42 +1,95 @@
+using System.Diagnostics;
+using Timer = System.Windows.Forms.Timer;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
-        private Button[,] buttons;
+        Stopwatch stopwatch = new Stopwatch();
         public Form1()
         {
-            int rows = 5;
-            int cols = 5;
-
-            int buttonWidth = 52;
-            int buttonHeight = 52;
-            int padding = 10;
-
             InitializeComponent();
+            Task1();
+            Task2();
+            Task3();
+        }
 
-            buttons = new Button[rows, cols];
+        public void Task1()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1;
+            timer.Tick += Task1_Tick;
+            timer.Start();
+            stopwatch.Start();
+        }
 
-            for (int i = 0; i < rows; i++)
+        private void Task1_Tick(object? sender, EventArgs e)
+        {
+            label1.Text = $"{stopwatch.ElapsedMilliseconds}";
+        }
+
+        private void Task2()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Task2_Tick;
+            timer.Start();
+            Task2_Tick(null, EventArgs.Empty);
+        }
+
+        private void Task2_Tick(object? sender, EventArgs e)
+        {
+            DateTime targetDate = new DateTime(2025, 1, 1, 0, 0, 0);
+            TimeSpan timeLeft = targetDate - DateTime.Now;
+
+            if (timeLeft.TotalSeconds > 0)
             {
-                for (int j = 0; j < cols; j++)
-                {
-                    buttons[i, j] = new Button();
-                    buttons[i, j].Text = i.ToString() + ", " + j.ToString();
-                    buttons[i, j].Size = new Size(buttonWidth, buttonHeight);
-                    buttons[i, j].Location = new Point(padding + (j * buttonWidth), padding + (i * buttonHeight));
-
-                    this.Controls.Add(buttons[i, j]);
-
-                    int x = i;
-                    int y = j;
-
-                    buttons[i, j].Click += (sender, e) =>
-                    {
-                        buttons[x, y].Enabled = false;
-                        buttons[x, y].Visible = false;
-                    };
-                }
+                label2.Text = $"{timeLeft.Days} days, {timeLeft.Hours} hours, {timeLeft.Minutes} minutes, {timeLeft.Seconds}, seconds";
             }
+            else
+            {
+                label2.Text = "Event has come";
+            }
+        }
+
+        public void Task3()
+        {
+            int clickCount = 0;        
+            int highScore = 0;      
+
+            Timer gameTimer = new Timer();
+            gameTimer.Interval = 1000;
+            int timeLeft = 20;
+
+            Controls.Add(button1);
+
+            button1.Click += (s, e) =>
+            {
+                if (gameTimer.Enabled)
+                {
+                    clickCount++;
+                }
+            };
+
+            gameTimer.Tick += (s, e) =>
+            {
+                timeLeft--;
+
+                if (timeLeft <= 0)
+                {
+                    gameTimer.Stop();
+
+                    if (clickCount > highScore)
+                    {
+                        highScore = clickCount;
+                    }
+                    
+                    MessageBox.Show($"Time's up!\nClicks: {clickCount}\nHigh Score: {highScore}", "Game Over");
+                    clickCount = 0;
+                    timeLeft = 20;
+                }
+            };
+            gameTimer.Start();
         }
     }
 }
